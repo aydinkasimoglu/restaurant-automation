2ad124ea5956987efeaa2218f2e287774a261fc5
janties <com.kerem@gmail.com>
2023-03-23 23:03:21 +0300

    ikinci deneme

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+	let counter = useState('counter', () => 1)
+
+	var akgfj: number
+
+	const bird: string = "kerem"
+
+
+	function increment() {
+		counter.value = counter.value + 1;
+	}
+	</script>
+
+	<template>
+	<div>
+		<button @click="() => {increment()}">{{ counter }}</button>
+		<button>{{ counter }}</button>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
