36741859920aec7afc2e6b984883e058c87c8ec7
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-03-27 22:11:19 +0300

    Logoya link eklendi

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,76 +1,73 @@
 <template>
-    <div>
-        <header>
-            <img src="/logo.png" alt="logo" />
+  <div>
+    <header>
+      <NuxtLink to="/"><img src="/logo.png" alt="logo" /></NuxtLink>
 
-            <nav role="navigation">
-                <NuxtLink to="/menu" class="nav-item">MENÜ</NuxtLink>
-                <NuxtLink to="/siparisler" class="nav-item"
-                    >SİPARİŞLER</NuxtLink
-                >
-                <NuxtLink to="/masalar" class="nav-item">MASALAR</NuxtLink>
-                <NuxtLink to="/stoklar" class="nav-item">STOKLAR</NuxtLink>
-                <NuxtLink to="/calisanlar" class="nav-item"
-                    >ÇALIŞANLAR</NuxtLink
-                >
-            </nav>
-        </header>
+      <nav role="navigation">
+        <NuxtLink to="/menu" class="nav-item">MENÜ</NuxtLink>
+        <NuxtLink to="/siparisler" class="nav-item">SİPARİŞLER</NuxtLink>
+        <NuxtLink to="/masalar" class="nav-item">MASALAR</NuxtLink>
+        <NuxtLink to="/stoklar" class="nav-item">STOKLAR</NuxtLink>
+        <NuxtLink to="/calisanlar" class="nav-item">ÇALIŞANLAR</NuxtLink>
+      </nav>
+    </header>
 
-        <div id="container">
-            <slot />
-        </div>
+    <div id="container">
+      <slot />
     </div>
+  </div>
 </template>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
 
 body {
-    display: flex;
-    flex-direction: column;
-    background-color: #f9f5e7;
+  display: flex;
+  flex-direction: column;
+  background-color: #f9f5e7;
 }
 
 header {
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-around;
-    align-items: center;
-    width: 100%;
-    position: relative;
-    margin-top: 2rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  margin-top: 2rem;
+  border: 1px solid black;
 }
 
 img {
-    width: 180px;
-    height: auto;
+  width: 180px;
+  height: auto;
 }
 
 nav {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    gap: clamp(2rem, 2vw, auto);
-    background-color: #f8ead8;
-    border-radius: 5px;
-    width: fit-content;
-    height: fit-content;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: clamp(2rem, 2vw, auto);
+  background-color: #f8ead8;
+  border-radius: 5px;
+  width: fit-content;
+  height: fit-content;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 }
 
 .nav-item {
-    cursor: pointer;
-    user-select: none;
-    font-family: "Roboto Condensed", sans-serif;
-    font-weight: bold;
-    text-decoration: none;
-    color: black;
-    padding: 1rem 2rem;
+  cursor: pointer;
+  user-select: none;
+  font-family: "Roboto Condensed", sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  padding: 1rem 2rem;
 }
 
 #container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
