acc27cede7f0d1f5ceb04e1d6fedeb7621a56daa
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-03-28 13:38:04 +0300

    ana sayfa içeriği tamamlandı

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
-<script lang="ts" setup>
-	
-</script>
-
 <template>
 	<div>
-		<p>Merhaba</p>
+		<h1>LOP'a Hoş Geldiniz!</h1>
+    <p>Menüye göz atabilir, siparişleri listeleyebilir, masaları kontrol edebilir ve daha fazlasını yapabilirsiniz!</p>
 	</div>
 </template>
 
 <style scoped>
-
+  div {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed", sans-serif;
+  }
 </style>
