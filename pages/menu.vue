119e8d545bdb2074e7892f808b38f8b7cee43ad0
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-04-06 21:05:06 +0300

    Menü içeriği görüntülenmesi eklendi

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,5 +1,23 @@
+<script lang="ts" setup>
+  const { data: yiyecekler, pending, error } = await useFetch('/api/menu')
+
+  if (error) {
+    console.log(`Menü yüklerken hata oluştu: ${error.value?.message}`)
+  }
+</script>
+
 <template>
-  <div>
-    Menü sayfası
+  <div v-if="error">
+    Bir hata oluştu
+  </div>
+  <div v-else-if="pending">
+    Yükleniyor...
+  </div>
+  <div v-else>
+    <div v-for="yiyecek of yiyecekler" :key="yiyecek.ad">
+      <p>{{ yiyecek.ad }}</p>
+      <p>{{ yiyecek.fiyat }}</p>
+      <p>{{ yiyecek.tur }}</p>
+    </div>
   </div>
 </template>
