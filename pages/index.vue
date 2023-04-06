e5734b8094d599e8b4ebd4d2d7f54e6779c6a52c
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-04-06 21:04:34 +0300

    Ana sayfa yüklenince veri tabanı kontrol ediliyor

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <script lang="ts" setup>
-	
+  let dbCreated = useCookie<boolean>('dbCreated', { default: () => false })
+
+  if (!dbCreated.value) {
+    const { data } = await useFetch('/api/loadDB', { server: false })
+    dbCreated.value = data.value as boolean
+  }
 </script>
 
 <template>
 	<div>
-		<p>Merhaba</p>
+		<h1>LOP'a Hoş Geldiniz!</h1>
+    <p>Menüye göz atabilir, siparişleri listeleyebilir, masaları kontrol edebilir ve daha fazlasını yapabilirsiniz!</p>
 	</div>
 </template>
 
 <style scoped>
-
+  div {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed", sans-serif;
+  }
 </style>
