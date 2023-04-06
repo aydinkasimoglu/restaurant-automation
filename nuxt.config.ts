14c7bb29318c4a5a7cabd1b5b7885656af9c31fb
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-04-06 20:18:49 +0300

    runtimeConfig ve head ayarlandı

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  runtimeConfig: {
+    user: process.env.sqlUser,
+    password: process.env.sqlPassword
+  },
+  app: {
+    head: {
+      title: 'LOP',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      htmlAttrs: {
+        lang: 'tr'
+      },
+    },
+  }
 })
