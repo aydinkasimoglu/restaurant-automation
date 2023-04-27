92eb987684fe6c6d06cc5108c42200dfd80fc39b
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-04-27 20:52:55 +0300

    Layouts klasörü yerine app.vue'ye geçildi

---
+++ app.vue
@@ -0,0 +1,82 @@
+<script lang="ts" setup>
+  let dbCreated = useCookie<boolean>('dbCreated', { default: () => false, maxAge: 60 * 60 * 24 * 365 })
+
+  if (!dbCreated.value) {
+    const { data } = await useFetch('/api/loadDB', { server: false })
+    dbCreated.value = data.value as boolean
+  }
+</script>
+
+<template>
+  <header>
+    <NuxtLink to="/"><img src="/logo.png" alt="logo" /></NuxtLink>
+
+    <nav role="navigation">
+      <NuxtLink to="/menu" class="nav-item">MENÜ</NuxtLink>
+      <NuxtLink to="/siparisler" class="nav-item">SİPARİŞLER</NuxtLink>
+      <NuxtLink to="/masalar" class="nav-item">MASALAR</NuxtLink>
+      <NuxtLink to="/stoklar" class="nav-item">STOKLAR</NuxtLink>
+      <NuxtLink to="/calisanlar" class="nav-item">ÇALIŞANLAR</NuxtLink>
+    </nav>
+  </header>
+
+  <div id="container">
+    <NuxtPage/>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
+
+body {
+  overflow: hidden;
+  height: 100vh;
+  background-color: #f9f5e7;
+}
+
+header {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  margin-top: 2rem;
+}
+
+img {
+  width: 180px;
+  height: auto;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: clamp(2rem, 2vw, auto);
+  background-color: #f8ead8;
+  border-radius: 5px;
+  width: fit-content;
+  height: fit-content;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+}
+
+.nav-item {
+  cursor: pointer;
+  user-select: none;
+  font-family: "Roboto Condensed", sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  padding: 1rem 2rem;
+}
+
+#container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+  height: calc(100vh - 9rem);
+}
+</style>
