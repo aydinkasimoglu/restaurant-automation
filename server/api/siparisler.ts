b87f07e260ff32c19ade20e5d3ffde88affd4d7a
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-04-28 22:44:05 +0300

    Tüm siparişleri döndüren server route

---
+++ server/api/siparisler.ts
@@ -0,0 +1,45 @@
+import mysql from "mysql"
+
+type Siparis = {
+  siparis_no: number
+  adet: number
+  ad: string
+  tur: string
+  tarih: string
+  fiyat: number
+  fotograf: string
+}
+
+export default defineEventHandler(async event => {
+  const connection: mysql.Connection = event.context.connection
+
+  try {
+    const siparisler = await getSiparislerList(connection) as Siparis[]
+
+    return siparisler
+  } catch (error) {
+    console.error(error)
+
+    throw new Error("Siparişler listelenemedi")
+  }
+})
+
+function getSiparislerList(connection: mysql.Connection) {
+  return new Promise((resolve, reject) => {
+    connection.query("USE Restoran", error => {
+      if (error) throw error
+      console.log("Veri tabanı kullanıldı")
+
+      connection.query(`
+        SELECT Siparis.siparis_no AS siparis_no, Siparis.adet AS adet, Yiyecek.ad AS ad, Yiyecek.fiyat AS fiyat, YiyecekTur.ad AS tur, Siparis.siparis_tarih AS tarih, Yiyecek.fotograf AS fotograf
+        FROM Siparis
+        JOIN Yiyecek ON Siparis.yiyecek_id = Yiyecek.yiyecek_id
+        JOIN YiyecekTur ON Yiyecek.tur_id = YiyecekTur.yiyecektur_id
+      `, (error, results) => {
+        if (error) reject(error)
+        console.log("Siparişler listelendi")
+        resolve(results)
+      })
+    })
+  })
+}
