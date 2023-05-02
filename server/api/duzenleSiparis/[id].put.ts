d1580fa23cf44d99061abc4fa6de8d058c2b3890
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-05-02 23:22:56 +0300

    Siparişleri düzenlemeye yarayan server route

---
+++ server/api/duzenleSiparis/[id].put.ts
@@ -0,0 +1,42 @@
+import mysql from "mysql"
+
+type Body = {
+  siparisId: number
+  siparisAd: string
+  siparisAdet: number
+  siparisNo: number
+}
+
+export default defineEventHandler(async event => {
+  const body: Body = await readBody(event)
+  const connection: mysql.Connection = event.context.connection
+
+  try {
+    const siparis = await updateSiparis(connection, body) as number
+
+    return siparis
+  } catch(error) {
+    console.error(error)
+
+    throw new Error("Sipariş güncellenemedi")
+  }
+})
+
+function updateSiparis(connection: mysql.Connection, body: Body) {
+  return new Promise((resolve, reject) => {
+    connection.query("USE Restoran", error => {
+      if (error) throw error
+      console.log("Veri tabanı kullanıldı")
+
+      connection.query(`
+        UPDATE Siparis
+        SET adet = ${body.siparisAdet}, yiyecek_id = (SELECT yiyecek_id FROM Yiyecek WHERE ad = '${body.siparisAd}'), siparis_no = ${body.siparisNo}
+        WHERE siparis_id = ${body.siparisId}`
+      , (error, results) => {
+        if (error) reject(error)
+        console.log("Sipariş güncellendi")
+        resolve(results.affectedRows)
+      })
+    })
+  })
+}
