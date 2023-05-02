37ec732b3b1bfe9cc0e4664efe47ac76843df02f
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-05-02 23:22:30 +0300

    Siparişlere düzenleme butonu eklendi

--- pages/siparisler/index.vue
+++ pages/siparisler/index.vue
@@ -1,29 +1,117 @@
 <script lang="ts" setup>
-  const { data: siparisler, pending, error } = await useFetch('/api/siparisler')
+  const { data: siparisler, pending, error: siparisError } = await useFetch('/api/siparisler')
+  const { data: yiyecekler, error: menuError } = await useFetch('/api/menu')
 
-  if (error.value !== null) {
-    console.error(`Menü yüklerken hata oluştu: ${error.value.message}`)
+  if (siparisError.value !== null) {
+    console.error(`Menü yüklerken hata oluştu: ${siparisError.value.message}`)
   }
 
-  const sil = async (id: number) => {
-    console.log(id)
-    if (confirm("Gerçekten silmek istiyor musun?")) {
-      const { data, error } = await useFetch(`/api/silSiparis/${id}`, {
-        method: 'DELETE'
+  if (menuError.value !== null) {
+    console.error(`Menü yüklerken hata oluştu: ${menuError.value.message}`)
+  }
+
+  let sil: (id: number) => Promise<void>
+  let duzenleAc: (siparisId: number, siparisNo: number, siparisAdet: number, siparisAd: string) => void
+  let duzenleKapat: () => void
+  let duzenle: (id: number) => Promise<void>
+
+  const id = useState('id', () => 0)
+  
+  onMounted(() => {
+    const edit = document.getElementById("siparis-edit")
+    const siparisNoInput = document.getElementById("siparis-no")
+    const siparisAdetInput = document.getElementById("siparis-adet")
+    const siparisAdInput = document.getElementById("siparis-ad")
+    const save = document.getElementById("siparis-kaydet")
+    const close = document.getElementById("edit-kapat")
+    
+    if (save) {
+      save.addEventListener('click', () => duzenle(id.value))
+    }
+
+    if (close) {
+      close.addEventListener('click', () => duzenleKapat())
+    }
+
+    sil = async (id: number) => {
+      console.log(id)
+      if (confirm("Gerçekten silmek istiyor musun?")) {
+        const { data, error } = await useFetch(`/api/silSiparis/${id}`, {
+          method: 'DELETE'
+        })
+
+        if (error.value !== null) {
+          console.error(`Sipariş silinirken hata oluştu: ${error.value.message}`)
+        } else {
+          alert(`${id} id'li sipariş silindi`)
+          location.reload()
+        }
+      }
+    }
+
+    duzenleAc = (siparisId: number, siparisNo: number, siparisAdet: number, siparisAd: string) => {
+      if (siparisNoInput === null || siparisAdetInput === null || siparisAdInput === null || edit === null) {
+        console.error('Sipariş düzenleme alanları bulunamadı')
+        return
+      }
+      (siparisNoInput as HTMLInputElement).value = siparisNo.toString();
+      (siparisAdetInput as HTMLInputElement).value = siparisAdet.toString();
+      (siparisAdInput as HTMLSelectElement).value = siparisAd
+      id.value = siparisId
+
+      edit.classList.remove('edit-kapali')
+      edit.classList.add('edit-acik')
+    }
+
+    duzenleKapat = () => {
+      if (edit === null) {
+        console.error('Sipariş düzenleme alanları bulunamadı')
+        return
+      }
+      edit.classList.remove('edit-acik')
+      edit.classList.add('edit-kapali')
+    }
+
+    duzenle = async (id: number) => {
+      const { data, error } = await useFetch(`/api/duzenleSiparis/${id}`, {
+        method: 'PUT',
+        body: {
+          siparisId: id,
+          siparisNo: (siparisNoInput as HTMLInputElement).value,
+          siparisAdet: (siparisAdetInput as HTMLInputElement).value,
+          siparisAd: (siparisAdInput as HTMLSelectElement).value
+        }
       })
 
       if (error.value !== null) {
-        console.error(`Sipariş silinirken hata oluştu: ${error.value.message}`)
+        console.error(`Sipariş düzenlenirken hata oluştu: ${error.value.message}`)
       } else {
-        alert(`${id} id'li sipariş silindi`)
+        alert(`${id} id'li sipariş düzenlendi`)
         location.reload()
       }
     }
-  }
+  })
 </script>
 
 <template>
   <div style="display: flex; flex-direction: column; align-items: flex-end;">
+    <div id="siparis-edit" class="edit-kapali">
+      <button type="button" id="edit-kapat" title="Düzenleme Alanını Kapat"><i class="fa-solid fa-times"></i></button>
+      <label for="siparis-no">Sipariş Numarası</label>
+      <input type="text" id="siparis-no" placeholder="Sipariş Numarası" />
+
+      <label for="siparis-adet">Sipariş Adedi</label>
+      <input type="number" id="siparis-adet" placeholder="Sipariş Adedi" />
+
+      <label for="siparis-ad">Sipariş Adı</label>
+      <select id="siparis-ad" name="siparisAd" title="Yiyecek Adı" required>
+        <option disabled>Yiyecek Adı</option>
+        <option v-for="yiyecek in yiyecekler" :key="yiyecek.ad">{{ yiyecek.ad }}</option>
+      </select>
+
+      <button type="button" id="siparis-kaydet">Kaydet</button>
+    </div>
+
     <NuxtLink to="/siparisler/yeni" id="yeni-siparis">Yeni Sipariş</NuxtLink>
 
     <div v-if="error">
@@ -46,6 +134,7 @@
         </div>
         <div>{{ siparis.fiyat }}₺</div>
         <button type="button" class="siparis-sil" title="Siparişi Sil" @click="sil(siparis.siparis_id)"><i class="fa-solid fa-trash"></i></button>
+        <button type="button" class="siparis-duzenle" title="Siparişi Düzenle" @click="duzenleAc(siparis.siparis_id, siparis.siparis_no, siparis.adet, siparis.ad)"><i class="fa-solid fa-pen"></i></button>
       </div>
     </div>
 
@@ -66,7 +155,7 @@
 
 .siparis {
   display: grid;
-  grid-template-columns: 1fr 2fr 0.4fr 0.4fr;
+  grid-template-columns: 1fr 2fr 0.4fr 0.4fr 0.4fr;
   padding: 0.5rem;
   border-radius: 0.5rem;
   width: 100%;
@@ -110,7 +199,7 @@ img {
   border-radius: 0.5rem;
 }
 
-.siparis-sil {
+.siparis-sil, .siparis-duzenle, #edit-kapat {
   border: none;
   background-color: #EDDBC7;
   padding: 0.5em;
@@ -118,8 +207,31 @@ img {
   cursor: pointer;
 }
 
-.siparis-sil:hover {
+.siparis-sil:hover, .siparis-duzenle:hover, #edit-kapat:hover {
   background-color: #DCCBB8;
+}
+
+.edit-acik {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  border-radius: 0.5rem;
+  background-color: #EDDBC7;
+  position: absolute;
+  z-index: 1;
+}
+
+.edit-acik > *:not(:first-child) {
+  width: 100%;
+}
+
+.edit-kapali {
+  display: none;
+  position: absolute;
 }
 
 </style>
