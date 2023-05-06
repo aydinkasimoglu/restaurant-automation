b139b5f28b9c8820a8c9c0ea7c76c8f6fc10e864
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-05-06 15:36:17 +0300

    Karmaşıklık giderildi

--- server/api/masalar.ts
+++ server/api/masalar.ts
@@ -23,21 +23,24 @@ export default defineEventHandler(async event => {
 function getYiyeceklerList(connection: mysql.Connection) {
   return new Promise((resolve, reject) => {
     connection.query("USE Restoran", error => {
-      if (error) throw error
+      if (error) reject(error)
       console.log("Veri tabanı kullanıldı")
 
       connection.query(`
-        UPDATE Masa
-        LEFT JOIN (
-          SELECT Siparis.masa_id, SUM(Yiyecek.fiyat * Siparis.adet) AS toplam_fiyat
-          FROM Siparis
-          LEFT JOIN Yiyecek ON Siparis.yiyecek_id = Yiyecek.yiyecek_id
-          GROUP BY Siparis.masa_id
-        ) AS Calculated ON Masa.masa_id = Calculated.masa_id
-        SET Masa.toplam_fiyat = IFNULL(Calculated.toplam_fiyat, 0),
-            Masa.dolu = CASE WHEN IFNULL(Calculated.toplam_fiyat, 0) <> 0 THEN 1 ELSE 0 END
-      `, (error, updateResult) => {
+        SELECT Siparis.masa_id, SUM(Yiyecek.fiyat * Siparis.adet) AS toplam_fiyat
+        FROM Siparis
+        JOIN Yiyecek ON Siparis.yiyecek_id = Yiyecek.yiyecek_id
+        GROUP BY Siparis.masa_id`
+      , (error: mysql.MysqlError, result: {masa_id: number, toplam_fiyat: number}[]) => {
         if (error) reject(error)
+
+        result.forEach(masa => {
+          connection.query(`
+            UPDATE Masa
+            SET toplam_fiyat = ${masa.toplam_fiyat}, dolu = ${masa.toplam_fiyat !== 0 ? 1 : 0}
+            WHERE masa_id = ${masa.masa_id}
+          `, (error) => {if (error) reject(error)})
+        })
 
         connection.query(`
           SELECT masa_id AS id, dolu, toplam_fiyat
