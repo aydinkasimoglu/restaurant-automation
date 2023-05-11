13d6bad9318e1d674316000a1df61f773d3c8aa4
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-05-11 11:44:53 +0300

    Masa güncellemesi trigger ile çözüldü

--- server/api/masalar.ts
+++ server/api/masalar.ts
@@ -25,45 +25,10 @@ function getYiyeceklerList(connection: mysql.Connection) {
     connection.query("USE Restoran", error => {
       if (error) reject(error)
 
-      connection.query(`
-        SELECT Siparis.masa_id, SUM(Yiyecek.fiyat * Siparis.adet) AS toplam_fiyat
-        FROM Siparis
-        JOIN Yiyecek ON Siparis.yiyecek_id = Yiyecek.yiyecek_id
-        GROUP BY Siparis.masa_id`
-      , async (error: mysql.MysqlError, results: {masa_id: number, toplam_fiyat: number}[]) => {
+      connection.query("SELECT masa_id AS id, dolu, toplam_fiyat FROM Masa", (error, result) => {
         if (error) reject(error)
 
-        for (let i = 1; i <= 10; i++) {
-          connection.query(`SELECT COUNT(*) FROM Siparis WHERE masa_id = ${i}`, (error, countResult) => {
-            if (error) reject(error)
-
-            const count: number = countResult[0]["COUNT(*)"]
-
-            if (count === 0) {
-              connection.query(`
-                UPDATE Masa
-                SET toplam_fiyat = 0, dolu = 0
-                WHERE masa_id = ${i}
-              `, (error) => {if (error) reject(error)})
-            }
-
-            if (count > 0) {
-              connection.query(`
-                UPDATE Masa
-                SET toplam_fiyat = ${results.find(item => item.masa_id === i)?.toplam_fiyat}, dolu = 1
-                WHERE masa_id = ${i}
-              `, (error) => {if (error) reject(error)})
-            }
-
-            if (i === 10) {
-              connection.query("SELECT Masa.masa_id AS id, dolu, toplam_fiyat FROM Masa", (error, result) => {
-                if (error) reject(error)
-
-                resolve(result)
-              })
-            }
-          })
-        }
+        resolve(result)
       })
     })
   })
